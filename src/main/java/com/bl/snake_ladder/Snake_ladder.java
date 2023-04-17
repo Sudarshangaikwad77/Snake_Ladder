2a5d276e5cbb524953b04a0ceef3af4ed8882099
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-17 12:52:37 +0530

    added code to uc2 branch

--- src/main/java/com/bl/snake_ladder/Snake_ladder.java
+++ src/main/java/com/bl/snake_ladder/Snake_ladder.java
@@ -2,32 +2,27 @@ package com.bl.snake_ladder;
 import java.util.Scanner;
 public class Snake_ladder
 {
-        public static void main(String[] args)
-        {
-
-            System.out.println("Welcome To Snake & Ladder Simulator");
-
-            //Starting Positions;
-
+    //UC2
+        public static void main(String[] args) {
             int position = 0;
 
             int[][] SnakeLadder = {{15, 22}, {14, 7}, {77, 32}, {95, 10}, {50, 32}};
 
-            while (position < 100){
+            while (position < 100) {
 
                 Scanner ab = new Scanner(System.in);
                 System.out.println("Press Enter for Roll the Dice");
                 ab.nextLine();
 
 
-                int dice = (int)(Math.random() * 6) + 1;
-                System.out.println("You Got: "+ dice);
+                int dice = (int) (Math.random() * 6) + 1;
+                System.out.println("You Got: " + dice);
 
                 position += dice;
-                break;
+                System.out.println("You are at: " + dice);
+
 
             }
-
         }
     }
 
